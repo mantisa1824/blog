ffa1f60597030f435a81e09699276a615119c96e
mantisa1824 <mantisa1824@bk.ru>
2014-09-06 16:24:48 +0400

    first

---
+++ app/controllers/IndexController.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mantisa1824
+ * Date: 04.09.14
+ * Time: 12:34
+ */
+
+class IndexController extends BaseController{
+
+	public function getIndex(){
+		return View::make('index/home');
+	}
+
+
+} 
